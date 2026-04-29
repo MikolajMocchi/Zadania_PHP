52c342fe0623cb6cb1e9bd94d17a43d113ec4ca6
Mocchi <mikolajgolonka43@gmail.com>
2026-04-29 15:27:48 +0200

    Update index2.php

--- index2.php
+++ index2.php
@@ -27,3 +27,68 @@ if ($liczba % 2 === 0) {
 ?>
 
 zad.3
+<?php
+
+$liczba = 3;
+$liczba2 = 2;
+
+if ($liczba < $liczba2) {
+    echo "$liczba2";
+} elseif ($liczba > $liczba2) {
+    echo "$liczba";
+}
+
+?>
+
+zad.4
+<?php
+
+$liczba = 19;
+
+if ($liczba >= 18) {
+    echo "Jesteś pełnoletni.";
+} else {
+    echo "Nie jesteś jeszcze pełnoletni.";
+}
+
+?>
+
+zad.5
+<?php
+
+$liczba = 9;
+
+if ($liczba == 10) {
+    echo "6";
+} elseif ($liczba == 9) {
+    echo "5";
+}
+elseif ($liczba == 7) {
+    echo "4";
+}
+elseif ($liczba == 5) {
+    echo "3";
+}
+elseif ($liczba == 4) {
+    echo "2";
+}
+elseif ($liczba <= 4) {
+    echo "1";
+}
+
+?>
+
+zad.6
+<?php
+
+$liczba = 2024;
+
+if ($liczba % 4 == 0) {
+    echo "Rok przestępny";
+} else {
+    echo "Nie rok przestępny";
+}
+
+?>
+
+zad.7
