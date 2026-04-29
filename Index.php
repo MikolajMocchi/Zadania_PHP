f103ed555e44c6063e90be6ce97455ed9c3f54e1
Mocchi <mikolajgolonka43@gmail.com>
2026-04-29 14:36:49 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -18,16 +18,3 @@ echo $a + $b
 ?>
 
 zad 3
-<?php
-
-$liczba = 0;
-
-if ($liczba <= -1) {
-    echo "Liczba ujemna";
-} elseif ($liczba >= 1) {
-    echo "liczba dodatnia";
-} elseif ($liczba == 0) {
-    echo "liczba jest równa zero";
-}
-
-?>
