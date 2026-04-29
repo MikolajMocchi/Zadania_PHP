8a1e32f5f63ac3658571060952af5dbee60a168f
Mocchi <mikolajgolonka43@gmail.com>
2026-04-29 14:37:16 +0200

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,14 @@
+Zad.1
+<?php
+
+$liczba = 0;
+
+if ($liczba <= -1) {
+    echo "Liczba ujemna";
+} elseif ($liczba >= 1) {
+    echo "liczba dodatnia";
+} elseif ($liczba == 0) {
+    echo "liczba jest równa zero";
+}
+
+?>
