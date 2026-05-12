57f3075cd59c34be2e94a69038ae79c2e7a2a602
Mocchi <mikolajgolonka43@gmail.com>
2026-05-12 15:12:43 +0200

    Add files via upload

---
+++ skibidijaro/dexter1_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$name = 'Miku';
+$age = 'Dajo';
+$cotry = 'Gofry';
+
+echo "Nazywam się $name , mam $age lat i mieszkam w $cotry"
+?>
+</body>
+</html>
