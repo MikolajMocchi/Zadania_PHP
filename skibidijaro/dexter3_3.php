f1fafcca1610edd7f38fe28239d931ab1688cf3d
Mocchi <mikolajgolonka43@gmail.com>
2026-05-12 16:10:39 +0200

    Add files via upload

---
+++ skibidijaro/dexter3_3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$lista = ["Mikołaj", "Aleksander", "Jakub", "Steve Fox", "Jin"];
+foreach ($lista as $list) {
+    echo $list . "\n";
+}
+?>
+</body>
+</html>
