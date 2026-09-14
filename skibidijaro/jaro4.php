9289a983cee40fc9c71af5ab829fd9478a5b75c5
Mocchi <mikolajgolonka43@gmail.com>
2026-09-14 14:46:44 +0200

    Add files via upload

---
+++ skibidijaro/jaro4.php
@@ -0,0 +1,14 @@
+<?php
+/* zad.4.1 */
+$clorki = ["Blck", "yllow", "whte"];
+
+echo $clorki[abs(-2)];
+
+/* zad.4.2 */
+$product = [
+    'name' => 'jajko',
+    'price' => 420,
+    'in_stock' => 'tak'
+];
+
+echo $product['in_stock'];
